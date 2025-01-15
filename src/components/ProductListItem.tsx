21ebb63a50ca1d656083c5d81e6f75389f32a0de
Mostafa Zahran <mostafa.k.zahran@gmail.com>
2025-01-15 21:27:18 +0100

    fix types

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,17 @@
 import { StyleSheet } from 'react-native';
 import { Text, View, Image } from 'react-native';
 import Colors from '@/src/constants/Colors';
+import { Product } from '@/src/types'
 
+const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+type ProductListItemProps = {
+  product: Product
+}
 
-export default ProductListItem = ({product}) => {
+export default ProductListItem = ({product} : ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image}/>
+      <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image}/>
       <Text style={styles.title}> {product.name} </Text>
       <Text style={styles.price}> ${product.price} </Text>
     </View>
